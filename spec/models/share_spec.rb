a090af787e52635abaaea6101c5b64cdf918594a
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2017-06-06 15:29:54 -0400

    Add size as enum to share class.

--- spec/models/share_spec.rb
+++ spec/models/share_spec.rb
@@ -3,4 +3,6 @@ require 'rails_helper'
 RSpec.describe Share, type: :model do
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_uniqueness_of(:name) }
+
+  it { is_expected.to define_enum_for(:size).with(%i(full half)) }
 end
